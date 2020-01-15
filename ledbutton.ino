99b0e483267e7a0db2bc886c46cbca0ea08e2747
Nelly Barret <nelly.barret@etu.univ-lyon1.fr>
2020-01-15 17:57:40 +0100

    arduino code

---
+++ ledbutton.ino
@@ -0,0 +1,27 @@
+int buttons_value[] = {0, 0, 0, 0, 0, 0, 0};
+int buttons[] = {0, 1, 2, 3, 4, 5, 6};
+int leds[] = {7, 8, 9, 10, 11, 12, 13};
+
+void setup() {
+  // Serial.begin(9600); // open the serial port at 9600 bps:
+  for(int i = 0 ; i < 7 ; i++) {
+    pinMode(buttons[i], INPUT);
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for(int i = 0 ; i < 7 ; i++) {
+    buttons_value[i] = digitalRead(buttons[i]);
+    if(buttons_value[i] != 0) {
+      digitalWrite(leds[i], HIGH);
+      //delay(1000);
+      //digitalWrite(leds[i], LOW);
+      // Serial.print(buttons_value[i]);
+      
+    } else {
+      digitalWrite(leds[i], LOW);
+      
+    }
+  }
+}
